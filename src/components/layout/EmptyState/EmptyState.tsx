d8e80d273d5f6f2d25e211ba18835605d8251e7c
findsakshat <voteforsakshat@gmail.com>
2024-02-25 19:07:45 +0530

    fix: fixed eslint errors in empty state screen

--- src/components/layout/EmptyState/EmptyState.tsx
+++ src/components/layout/EmptyState/EmptyState.tsx
@@ -1,10 +1,10 @@
 export default function EmptyState() {
   return (
     <div className="flex flex-col justify-center items-center gap-4 text-center py-8">
-      <img height={150} width={340} src="empty-screen.svg" />
+      <img height={150} width={340} src="empty-screen.svg" alt="empty-state" />
       <div>
         <h5 className="text-xl font-medium text-gray-900 dark:text-white">Not Found</h5>
-        <p className="text-gray-500 text-sm">We can't find what you are looking for</p>
+        <p className="text-gray-500 text-sm">We can&apos;t find what you are looking for</p>
       </div>
     </div>
   )
